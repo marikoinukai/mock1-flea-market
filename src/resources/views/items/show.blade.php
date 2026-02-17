45071f925e9dba67dfb3fb0d2844330ca44b11bb
mariko inukai <mariko.inukai1006@gmail.com>
2026-02-18 00:17:45 +0900

    リモートリポジトリの変更

--- src/resources/views/items/show.blade.php
+++ src/resources/views/items/show.blade.php
@@ -4,6 +4,29 @@
 <p>価格：{{ $item->price }}</p>
 <p>出品者名：{{ $item->seller->name }}</p>
 <p>画像のパス：{{ $item->image->image_path }}</p>
+
+@if ($item->image)
+    <img src="{{ asset('img/'.$item->image->image_path) }}" alt="item">
+@endif
+
 @foreach ($item->categories as $category)
     <p>カテゴリ名：{{ $category->name }}</p>
 @endforeach
+
+<h2>コメント</h2>
+
+@if ($item->comments->isEmpty())
+    <p>コメントはまだありません</p>
+@else
+    @foreach ($item->comments as $comment)
+    <div style="display:flex; gap:10px; margin-bottom:12px;">
+      {{-- アイコン --}}
+      <img src="{{ asset('img/default-user.png') }}" alt="user" width="32" height="32">
+    </div>
+            <div>
+                <p>{{ $comment->user->name }}</p>
+                <p>{{ $comment->body }}</p>
+            </div>
+
+    @endforeach
+@endif
