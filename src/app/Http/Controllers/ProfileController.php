76cff6ed9d4946cb50c92cbd86e22e02a705f173
mariko inukai <mariko.inukai1006@gmail.com>
2026-02-22 21:30:17 +0900

    リモートリポジトリの変更

--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -14,22 +14,30 @@ class ProfileController extends Controller
 
     public function update(Request $request)
     {
+        /** @var \App\Models\User $user */
+        $user = auth()->user();
+
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'postal_code' => ['required', 'string', 'max:8'],
             'address_line1' => ['required', 'string', 'max:255'],
-            // address_line2 があるなら任意
             'address_line2' => ['nullable', 'string', 'max:255'],
-            // 画像は後で（今は一旦なし）
+            'icon' => ['nullable', 'image', 'max:2048'],
         ]);
 
-        $user = auth()->user();
-        $user->update([
+        $data = [
             'name' => $request->name,
             'postal_code' => $request->postal_code,
             'address_line1' => $request->address_line1,
             'address_line2' => $request->address_line2 ?? null,
-        ]);
+        ];
+
+        if ($request->hasFile('icon')) {
+            $path = $request->file('icon')->store('icons', 'public');
+            $data['icon_path'] = $path;
+        }
+
+        $user->update($data);
 
         return redirect()->route('items.index')->with('success', 'プロフィールを更新しました');
     }
--- src/resources/views/mypage/profile.blade.php
+++ src/resources/views/mypage/profile.blade.php
@@ -1,41 +1,64 @@
 @extends('layouts.app')
 
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
+@endsection
+
 @section('content')
     <div class="container">
         <h2>プロフィール設定</h2>
 
         @if ($errors->any())
-            <ul style="color:red;">
+            <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
                 @endforeach
             </ul>
         @endif
 
-        <form method="POST" action="{{ route('profile.update') }}">
+        <form method="POST" action="{{ route('profile.update') }}" enctype="multipart/form-data">
             @csrf
+
+            {{-- アイコン表示 --}}
+            <div class="profile-icon">
+                @if ($user->icon_path)
+                    <img src="{{ asset('storage/' . $user->icon_path) }}" alt="icon" class="profile-icon__img">
+                @else
+                    <div class="profile-icon__placeholder"></div>
+                @endif
+            </div>
+
+            {{-- アイコン選択 --}}
+            <div class="profile-field">
+                <label class="profile-label">アイコン画像</label>
+                <input type="file" name="icon" accept="image/*" class="profile-input-file">
+
+                @error('icon')
+                    <p class="profile-error">{{ $message }}</p>
+                @enderror
+            </div>
 
             <div>
                 <label>ユーザー名</label><br>
                 <input type="text" name="name" value="{{ old('name', $user->name) }}">
             </div>
 
-            <div style="margin-top:10px;">
+            <div>
                 <label>郵便番号</label><br>
                 <input type="text" name="postal_code" value="{{ old('postal_code', $user->postal_code) }}">
             </div>
 
-            <div style="margin-top:10px;">
+            <div>
                 <label>住所</label><br>
                 <input type="text" name="address_line1" value="{{ old('address_line1', $user->address_line1) }}">
             </div>
 
-            <div style="margin-top:10px;">
+            <div>
                 <label>建物名・部屋番号</label><br>
                 <input type="text" name="address_line2" value="{{ old('address_line2', $user->address_line2) }}">
             </div>
 
-            <button style="margin-top:14px;">更新する</button>
+            <button>更新する</button>
         </form>
     </div>
 @endsection
