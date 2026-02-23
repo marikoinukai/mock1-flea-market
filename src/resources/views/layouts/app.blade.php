c68dd96b287d15686ba985db186e535c9a007316
mariko inukai <mariko.inukai1006@gmail.com>
2026-02-24 00:15:40 +0900

    リモートリポジトリの変更

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -13,7 +13,9 @@
 <body>
     <header class="app-header">
         <div class="app-header__inner">
-            <a href="{{ route('items.index') }}" class="app-header__logo">Flea Market</a>
+            <a href="{{ route('items.index') }}" class="app-header__logo">
+                <img src="{{ asset('img/logo.png') }}" alt="logo" class="app-header__logoImg">
+            </a>
 
             <form action="{{ route('items.index') }}" method="GET" class="app-header__search">
                 <input type="text" name="keyword" value="{{ request('keyword') }}" placeholder="なにをお探しですか？"
@@ -23,23 +25,25 @@
 
             <nav class="app-header__nav">
                 @auth
-                    <a href="{{ route('profile.edit') }}" class="app-header__user">
-                        @if (auth()->user()->icon_path)
-                            <img src="{{ asset('storage/' . auth()->user()->icon_path) }}" alt="icon"
-                                class="app-header__avatar">
-                        @else
-                            <span class="app-header__avatar app-header__avatar--placeholder"></span>
-                        @endif
-                        <span class="app-header__userText">マイページ</span>
-                    </a>
 
-                    <form method="POST" action="{{ route('logout') }}" class="app-header__logout">
+                    <form method="POST" action="{{ route('logout') }}">
                         @csrf
-                        <button type="submit" class="ui-button ui-button--primary">ログアウト</button>
+                        <button type="submit" class="app-header__link">
+                            ログアウト
+                        </button>
                     </form>
+
+                    <a href="{{ route('profile.edit') }}" class="app-header__user">
+                        <span class="app-header__userText">マイページ</span>
+                        {{-- <button type="submit" class="ui-button app-header__searchBtn">出品</button> --}}
+                    </a>
+                    {{-- 出品（リンクにする） --}}
+                    <a href="{{ route('items.index') }}" class="ui-button ui-button--primary app-header__navBtn">
+                        出品
+                    </a>
                 @else
                     <a href="{{ route('login') }}" class="app-header__link">ログイン</a>
-                    <a href="{{ route('register') }}" class="app-header__link app-header__link--primary">会員登録</a>
+                    <a href="{{ route('register') }}" class="app-header__link">会員登録</a>
                 @endauth
             </nav>
         </div>
