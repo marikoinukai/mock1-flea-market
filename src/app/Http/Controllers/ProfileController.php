fa6df3abeb74579ae0734b065ef1e9569dce8645
mariko inukai <mariko.inukai1006@gmail.com>
2026-03-08 00:22:59 +0900

    リモートリポジトリの変更

--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -43,19 +43,11 @@ class ProfileController extends Controller
         $validated = $request->validated();
         $user = auth()->user();
 
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'postal_code' => ['required', 'string', 'max:8'],
-            'address_line1' => ['required', 'string', 'max:255'],
-            'address_line2' => ['nullable', 'string', 'max:255'],
-            'icon' => ['nullable', 'image', 'max:2048'],
-        ]);
-
         $data = [
-            'name' => $request->name,
-            'postal_code' => $request->postal_code,
-            'address_line1' => $request->address_line1,
-            'address_line2' => $request->address_line2 ?? null,
+            'name' => $validated['name'],
+            'postal_code' => $validated['postal_code'],
+            'address_line1' => $validated['address_line1'],
+            'address_line2' => $validated['address_line2'] ?? null,
         ];
 
         if ($request->hasFile('icon')) {
--- src/app/Http/Requests/ProfileRequest.php
+++ src/app/Http/Requests/ProfileRequest.php
@@ -13,8 +13,15 @@ class ProfileRequest extends FormRequest
 
     public function rules(): array
     {
+        $user = $this->user();
+
         return [
-            'icon' => ['required', 'file', 'mimes:jpeg,png'],
+            'icon' => [
+                $user && !$user->icon_path ? 'required' : 'nullable',
+                'file',
+                'mimes:jpeg,png'
+            ],
+
             'name' => ['required', 'string', 'max:20'],
             'postal_code' => ['required', 'regex:/^\d{3}-\d{4}$/'],
             'address_line1' => ['required', 'string', 'max:255'],
@@ -25,16 +32,18 @@ class ProfileRequest extends FormRequest
     public function messages(): array
     {
         return [
+            'icon.required' => 'プロフィール画像を選択してください。',
             'icon.mimes' => 'プロフィール画像はjpegまたはpng形式でアップロードしてください。',
 
-            'name.required' => 'ユーザー名は必須です。',
+            'name.required' => 'ユーザー名を入力してください。',
             'name.max' => 'ユーザー名は20文字以内で入力してください。',
 
-            'postal_code.required' => '郵便番号は必須です。',
+            'postal_code.required' => '郵便番号を入力してください。',
             'postal_code.regex' => '郵便番号はハイフンあり8文字で入力してください。',
 
-            'address_line1.required' => '住所は必須です。',
+            'address_line1.required' => '住所を入力してください。',
             'address_line1.max' => '住所は255文字以内で入力してください。',
+
             'address_line2.max' => '建物名は255文字以内で入力してください。',
         ];
     }
